d7ee79e04355e2bf54eec3cb396fe6ecc623ef73
Romario Lawes <romario.lawes@mymona.uwi.edu>
2017-11-15 19:38:44 +0000

    Lab 7

---
+++ world.js
@@ -0,0 +1,25 @@
+"use strict";
+window.onload = function() {
+    
+  function init() {
+    let xhr = new XMLHttpRequest();
+    
+    document.getElementById("lookup").addEventListener("click", function() {
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          document.getElementById("result").innerHTML = this.responseText;
+          // alert(this.responseText);
+        }
+      };
+      if (document.getElementById("all").checked) {
+        xhr.open("GET", "world.php?q=all=true", true);
+      } else {
+        xhr.open("GET", "world.php?q=" + document.getElementById("country").value, true);
+      }
+      xhr.send();
+    });
+    
+  }
+  
+  init();
+};
---
+++ world.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+$query = htmlentities($_GET["q"]);
+
+if ($query === "all=true") {
+  $stmt = $conn->query("SELECT * FROM countries");
+} else {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name = '$query'");
+}
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<ul>';
+foreach ($results as $row) {
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+}
+echo '</ul>';
